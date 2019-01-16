132e2e529a8233d67f720a20fdf7249e8bdc4155
Lydia Braga <lydiaccb@hotmail.com>
2019-01-16 21:39:25 -0200

    Adicionar arquivos de projeto.

---
+++ TesteRoslyn/Program.cs
@@ -0,0 +1,64 @@
+using static System.Console;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TesteRoslyn
+{
+    class Program
+    {
+
+        const string programText = @"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    [SimpleAttr(intProp: 5, stringProp: ""Hello World"")]
+    public class SimpleClassToAnalyze
+        {
+            public int MySimpleProperty { get; set; }
+
+            public event Action<object> MySimpleEvent;
+
+            public int MySimpleMethod(string str, out bool flag, int i = 5)
+            {
+                flag = true;
+
+                return 5;
+            }
+        }
+    }";
+
+        static void Main(string[] args)
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
+            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+
+            foreach(SyntaxNode node in root.DescendantNodes())
+            {
+                if (node.GetType() == typeof(AttributeListSyntax))
+                {
+                    AttributeListSyntax attr = (AttributeListSyntax) node;
+                    foreach (AttributeSyntax attribute in attr.Attributes)
+                    {
+                        WriteLine($"Attribute Name: {attribute.Name.ToString()}");
+
+                        AttributeArgumentListSyntax arguments = attribute.ArgumentList;
+
+                        foreach (AttributeArgumentSyntax argument in arguments.Arguments)
+                        {
+                            WriteLine($"Argument Name: {argument.NameColon.ToString()} - Value: {argument.Expression.ToString()}");
+                        }
+                    }
+                }
+            }
+
+            ReadLine();
+        }
+    }
+}
